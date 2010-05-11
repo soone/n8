2c9e863393ea8c05f87c435e14b4b80f5431510f
soone chen <fengyue15@163.com>
2010-05-11 23:04:18 +0800

    尝试使用单元测试

--- n8.php
+++ n8.php
@@ -8,5 +8,47 @@ define('VERSION', 'v0.0.1');
 define('N8ROOT', dirname(__FILE__));
 final class N8
 {
-	
+	/**
+	 * __autoload 
+	 * 
+	 * @static
+	 * @access public
+	 * @return void
+	 */
+	public function __autoload($cName)
+	{
+		$splFuncs = count(spl_autoload_functions());
+        $classPath = str_replace('_', '/', $cName . '.php');
+        if(file_exists($fullPath = PROJECT_DIR . '/' . $classPath) || file_exists($fullPath = LIBRARY . '/' . $classPath))
+        {
+        	include_once($fullPath);
+        }
+        else if($splFuncs == 1)
+        {
+        	include_once LIBRARY . '/LoadException.php';
+        	eval("class $className{}");
+        	throw new LoadException('请求的类：' . $className . '不存在', 404);
+        }
+                                                                                                                           
+        if(!class_exists($className) && $splFuncs == 1)
+        {
+        	include_once LIBRARY . '/LoadException.php';
+        	eval("class $className{}");
+        	throw new LoadException('请求的类：' . $className . '不存在', 404);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
 }
---
+++ unit/n8Test.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+
+class n8Test extends PHPUnit_Framework_TestCase
+{
+}
